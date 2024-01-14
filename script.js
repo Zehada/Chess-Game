64251af3895d5bcc5ecc7250b5cde9265c94a0e5
Oguzhan Oncu <oguzhan.oncu7@gmail.com>
2024-01-14 13:41:53 +0100

    Box names ok

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
 const chessboard = document.querySelector(".chessboard");
 
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+let number = 8;
 
 
 for (let i = 0; i < 8; i++) {
+
     if (i % 2 == 0) {
-        for (let i = 0; i < 8; i++) {
-            if (i % 2 == 0) {
+        for (let j = 0; j < 8; j++) {
+            if (j % 2 == 0) {
                 let square = document.createElement('div');
-                square.style.backgroundColor = "red";
+                square.classList.add(letters[j] + (number));
+                square.style.backgroundColor = "white";
                 chessboard.appendChild(square);
 
             } else {
                 let square = document.createElement('div');
-                square.style.backgroundColor = "blue";
+                square.classList.add(letters[j] + (number));
+                square.style.backgroundColor = "brown";
                 chessboard.appendChild(square);
             }
         }
     } else {
-        for (let i = 0; i < 8; i++) {
-            if (i % 2 > 0) {
+        for (let j = 0; j < 8; j++) {
+            if (j % 2 > 0) {
                 let square = document.createElement('div');
-                square.style.backgroundColor = "red";
+                square.classList.add(letters[j] + (number));
+                square.style.backgroundColor = "white";
                 chessboard.appendChild(square);
 
             } else {
                 let square = document.createElement('div');
-                square.style.backgroundColor = "blue";
+                square.classList.add(letters[j] + (number));
+                square.style.backgroundColor = "brown";
                 chessboard.appendChild(square);
             }
         }
 
     }
+
+    number--;
+
 }
